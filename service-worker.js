de825cd4bf1f507cf70d5a41de362c78450979b2
lkkuai <lkkuai@gmail.com>
2016-02-17 17:30:08 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,19 +5,13 @@ self.addEventListener('push', function(event) {
   var body = '發送訊息3';  
   var icon = 'http://udn.com/static/img/logo.png?20160108';  
   var tag = 'simple-push-demo-notification-tag';
-  var data = {
-      news: {
-          title: 'test notify'
-          desc: ' description'
-      }
-    };
+
 
   event.waitUntil(  
     self.registration.showNotification(title, {  
       body: body,  
       icon: icon,  
-      tag: tag,
-      data: data
+      tag: tag
     })  
   );  
 });
